1f8670d00fbf60db3516cf3aadb23c2c2a586596
hzz0603 <huzhan0603@outlook.com>
2026-07-02 02:33:23 +0000

    sysupgrade: add support for Ruijie RG-MA3063

--- target/linux/qualcommax/ipq50xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq50xx/base-files/lib/upgrade/platform.sh
@@ -179,6 +179,11 @@ platform_pre_upgrade() {
 
 platform_do_upgrade() {
 	case "$(board_name)" in
+	ruijie,rg-ma3063)
+		CI_UBIPART="firmware"
+		
+		nand_do_upgrade "$1"
+		;;
 	cmcc,pz-l8|\
 	elecom,wrc-x3000gs2|\
 	iodata,wn-dax3000gr)
